f4dc787fce2352e0a8d8ce4de5d04491a0478acf
OnlynfK <onlygod15@hotmail.com>
2024-01-29 11:33:56 +0100

    test deployment

--- src/Pages/DashboardPage/MyOrders/MyOrders.js
+++ src/Pages/DashboardPage/MyOrders/MyOrders.js
@@ -37,23 +37,24 @@ export const orderStatusMapping = {
 
 const Card = ({ title, Icon, quantity, className }) => {
   return (
-    <div className="flex h-full">
-      <div className="flex items-center border border-gray-200 w-full rounded-lg p-4">
-        <div
-          className={`flex items-center justify-center p-3 rounded-full h-12 w-12 text-xl text-center mr-4 ${className}`}
-        >
-          <Icon />
-        </div>
-        <div>
-          <h5 className="leading-none mb-2 text-base font-medium font-serif text-gray-700">
-            {title}
-          </h5>
-          <p className="text-xl font-bold font-serif leading-none text-gray-800">
-            {quantity}
-          </p>
-        </div>
+    <div className="d-flex h-100">
+    <div className="d-flex align-items-center border border-gray-200 w-100 rounded-lg p-4">
+      <div
+        className={`d-flex align-items-center justify-content-center p-3 rounded-circle h-12 w-12 text-xl text-center mr-4 ${className}`}
+      >
+        <Icon />
+      </div>
+      <div>
+        <h5 className="m-0 mb-2 text-base font-weight-medium font-serif text-gray-700">
+          {title}
+        </h5>
+        <p className="m-0 text-xl font-weight-bold font-serif text-gray-800">
+          {quantity}
+        </p>
       </div>
     </div>
+  </div>
+  
   );
 };
 
